57a8a9df8dadf64b36e7034c936d4683d008028c
OlenaPletinka <alenuwka05.88@gmail.com>
2021-06-10 12:24:49 +0300

    add liquibase

--- src/main/java/com/pharmacy/pharmacy/PharmacyApplication.java
+++ src/main/java/com/pharmacy/pharmacy/PharmacyApplication.java
@@ -1,13 +1,25 @@
 package com.pharmacy.pharmacy;
 
+
+import liquibase.exception.LiquibaseException;
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PharmacyApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(PharmacyApplication.class, args);
+  public static void main(String[] args) throws LiquibaseException {
+    ConfigurableApplicationContext context = SpringApplication.run(PharmacyApplication.class, args);
+
+    SpringLiquibase liquibase = new SpringLiquibase();
+    liquibase.setDataSource(context.getBean(DataSource.class));
+    liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.xml");
+    liquibase.setShouldRun(true);
+    liquibase.afterPropertiesSet();
 
   }
 
